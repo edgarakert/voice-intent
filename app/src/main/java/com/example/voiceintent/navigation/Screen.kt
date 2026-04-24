77adec9cacf58b2b5b86fdbebd4a5da62b0d5213
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-04-24 17:33:36 +0300

    #13 Правки в навигации

--- app/src/main/java/com/example/voiceintent/navigation/Screen.kt
+++ app/src/main/java/com/example/voiceintent/navigation/Screen.kt
@@ -1,12 +1,18 @@
 package com.example.voiceintent.navigation
 
+import android.net.Uri
+
 sealed class Screen(val route: String) {
-    object Notes : Screen(route = "notes")
-    object Record : Screen(route = "record")
-    object Details : Screen(route = "detail/{noteId}") {
+    data object Notes : Screen(route = "notes")
+    data object Record : Screen(route = "record")
+    data object Details : Screen(route = "detail/{noteId}") {
         fun createRoute(noteId: Long) = "detail/$noteId"
     }
 
-    object Analytics : Screen(route = "analytics")
-    object Settings : Screen(route = "settings")
+    data object NoteAnalysis : Screen(route = "note_analysis/{audio_path}/{language}") {
+        fun createRoute(audioPath: String, language: String): String =
+            "note_analysis/${Uri.encode(audioPath)}/$language"
+    }
+
+    data object Settings : Screen(route = "settings")
 }
