21ea919c7389c61f4b911e7a704b55ef60fb2043
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-05 21:11:38 +0300

    #14 Правки в репозитории

--- app/src/main/java/com/example/voiceintent/feature/note/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/example/voiceintent/feature/note/data/repository/NoteRepositoryImpl.kt
@@ -8,6 +8,7 @@ import com.example.voiceintent.feature.note.domain.entity.Note
 import com.example.voiceintent.feature.note.domain.repository.NoteRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -20,11 +21,10 @@ class NoteRepositoryImpl @Inject constructor(
         return@withContext dao.insert(note.toEntity())
     }
 
-    override fun getAllNotes(): Flow<List<Note>> {
-        return dao.getAllNotes().map { list ->
-            list.map { it.toDomain() }
-        }
-    }
+    override fun getAllNotes(): Flow<List<Note>> = dao.getAllNotes().map { list ->
+        list.map { it.toDomain() }
+    }.flowOn(ioDispatcher)
+
 
     override suspend fun getNoteById(id: Long): Note? = withContext(ioDispatcher) {
         return@withContext dao.getNoteById(id)?.toDomain()
