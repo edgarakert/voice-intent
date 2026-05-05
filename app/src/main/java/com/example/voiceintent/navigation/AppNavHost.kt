eb0fabc8e7560069bd2660381b8ea8079e234f5c
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-05 20:36:33 +0300

    #14 Обновил навигацию

--- app/src/main/java/com/example/voiceintent/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/voiceintent/navigation/AppNavHost.kt
@@ -3,10 +3,8 @@ package com.example.voiceintent.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.example.voiceintent.feature.note_analysis.presentation.screen.NoteAnalysisScreen
 import com.example.voiceintent.feature.record.presentation.screen.RecordScreen
 import com.example.voiceintent.feature.record.presentation.service.RecordControl
@@ -22,16 +20,15 @@ fun AppNavHost(
         startDestination = Screen.Record.route,
         modifier = modifier
     ) {
-        composable(route = Screen.Record.route) {
+        composable(
+            route = Screen.Record.route
+        ) {
             RecordScreen(
                 recordControl = recordControl,
                 navigateBack = { navController.popBackStack() },
-                onRecordingDone = { audioRecord ->
+                onRecordingDone = {
                     navController.navigate(
-                        Screen.NoteAnalysis.createRoute(
-                            audioPath = audioRecord.path,
-                            language = audioRecord.language.code
-                        )
+                        Screen.NoteAnalysis.route
                     )
                 },
             )
@@ -39,18 +36,12 @@ fun AppNavHost(
 
         composable(
             route = Screen.NoteAnalysis.route,
-            arguments = listOf(
-                navArgument("audio_path") { type = NavType.StringType },
-                navArgument("language") { type = NavType.StringType }
-            )
         ) {
             NoteAnalysisScreen(
                 navigateBack = { navController.popBackStack() },
                 onAnalysisDone = {
                     navController.navigate(Screen.Notes.route) {
-                        popUpTo(Screen.Record.route) {
-                            inclusive = false
-                        }
+                        popUpTo(Screen.Record.route) { inclusive = true }
                     }
                 }
             )
