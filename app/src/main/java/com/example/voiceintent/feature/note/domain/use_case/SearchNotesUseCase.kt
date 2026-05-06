d8cd92de4a2a5f3b1e49d446d68d3ca14f2b3f0b
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-06 11:55:58 +0300

    #15 Создал Usecase'ы

---
+++ app/src/main/java/com/example/voiceintent/feature/note/domain/use_case/SearchNotesUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.voiceintent.feature.note.domain.use_case
+
+import com.example.voiceintent.feature.note.domain.entity.Note
+import com.example.voiceintent.feature.note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchNotesUseCase @Inject constructor(
+    private val repository: NoteRepository
+) {
+    operator fun invoke(query: String): Flow<List<Note>> = repository.searchNotes(query = query)
+}
