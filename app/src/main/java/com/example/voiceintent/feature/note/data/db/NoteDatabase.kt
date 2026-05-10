85cb51e33e368db7d6a5ead84cc9ceba791ebc10
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-10 10:18:06 +0300

    #16 Создал отдельный TagDao

--- app/src/main/java/com/example/voiceintent/feature/note/data/db/NoteDatabase.kt
+++ app/src/main/java/com/example/voiceintent/feature/note/data/db/NoteDatabase.kt
@@ -4,13 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.voiceintent.feature.note.data.db.converter.StringListConverter
+import com.example.voiceintent.feature.tag.data.db.NoteTagEntity
+import com.example.voiceintent.feature.tag.data.db.TagDao
+import com.example.voiceintent.feature.tag.data.db.TagEntity
 
 @Database(
-    entities = [NoteEntity::class],
-    version = 1,
+    entities = [NoteEntity::class, TagEntity::class, NoteTagEntity::class],
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(StringListConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
+
+    abstract fun tagDao(): TagDao
 }
