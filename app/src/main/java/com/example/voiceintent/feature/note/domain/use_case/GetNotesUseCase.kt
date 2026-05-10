ba8182f2019bcd0a88872a6cb2a4ffda9a9285e6
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-10 10:20:50 +0300

    #16 Обновил usecase

--- app/src/main/java/com/example/voiceintent/feature/note/domain/use_case/GetNotesUseCase.kt
+++ app/src/main/java/com/example/voiceintent/feature/note/domain/use_case/GetNotesUseCase.kt
@@ -2,11 +2,17 @@ package com.example.voiceintent.feature.note.domain.use_case
 
 import com.example.voiceintent.feature.note.domain.entity.Note
 import com.example.voiceintent.feature.note.domain.repository.NoteRepository
-import kotlinx.coroutines.flow.Flow
+import com.example.voiceintent.feature.note_analysis.domain.entity.Mood
 import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
 
 class GetNotesUseCase @Inject constructor(
     private val repository: NoteRepository
 ) {
-    operator fun invoke(): Flow<List<Note>> = repository.getAllNotes()
+    operator fun invoke(
+        query: String = "",
+        mood: Mood? = null,
+        offset: Int = 0
+    ): Flow<List<Note>> =
+        repository.getNotes(query, mood, offset)
 }
